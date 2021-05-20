81c5ca2c7dd5de17445305be4a987d9369f839e5
Himanshu <himrd95@gmail.com>
2021-05-20 11:30:57 +0530

    patterPrintingV.js

--- patternPrintingV.js
+++ patternPrintingV.js
@@ -6,7 +6,7 @@ function runProgram(input) {
             if (j <= input*2-i*2 +2 && j % 2 ===1) {
                 res += "*"
             }
-            else if(j !== input*2-1) {
+            else {
                 res += " "
             }
         }
@@ -34,3 +34,44 @@ else{
     process.exit(0);
     });
 }
+
+
+
+
+
+// function runProgram(input) {
+//     input = +input;
+//     for (var i = 1; i <= input; i++){
+//         var res = ""
+//         for (var j = 1; j < input*2; j++){
+//             if (j <= input*2-i*2 +2 && j % 2 ===1) {
+//                 res += "*"
+//             }
+//             else if(j !== input*2-1) {
+//                 res += " "
+//             }
+//         }
+//         console.log(res)
+//     }
+// }
+
+// if(process.env.USERNAME === "Himanshu"){
+//     runProgram(`5`);
+// }
+// else{
+//     process.stdin.resume();
+//     process.stdin.setEncoding("ascii");
+//     let read = "";
+//     process.stdin.on("data", function (input) {
+//     read += input;
+//     });
+//     process.stdin.on("end", function () {
+//     read = read.replace(/\n$/,"")
+//     runProgram(read);
+//     });
+//     process.on("SIGINT", function () {
+//     read = read.replace(/\n$/,"")
+//     runProgram(read);
+//     process.exit(0);
+//     });
+// }
