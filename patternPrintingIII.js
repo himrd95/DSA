7529f63970d175e4affe422843ff6c35638a1c6d
Himanshu <himrd95@gmail.com>
2021-05-25 23:22:47 +0530

    new problem added

---
+++ patternPrintingIII.js
@@ -0,0 +1,39 @@
+function runProgram(input) {
+    input = +input;
+    for (var i = 1; i <= input; i++){
+        var res = ""
+        for (var j = 1; j <= input; j++){
+            if (i === 1 || i === input) {
+                res += "* "
+            } else {
+                if (j === input) {
+                    res += "* "
+                } else {
+                    res += "  "
+                }
+            }
+        }
+        console.log(res)
+    }
+}
+
+if(process.env.USERNAME === "Himanshu"){
+    runProgram(`5`);
+}
+else{
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+    read += input;
+    });
+    process.stdin.on("end", function () {
+    read = read.replace(/\n$/,"")
+    runProgram(read);
+    });
+    process.on("SIGINT", function () {
+    read = read.replace(/\n$/,"")
+    runProgram(read);
+    process.exit(0);
+    });
+}
