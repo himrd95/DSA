733f05e67fafa23d8e76135e424f552e76869833
Himanshu <himrd95@gmail.com>
2021-06-25 15:44:51 +0530

    fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,34 @@
+function getRes(num) {
+    if (num <= 1) {
+        return num;
+    }
+    return getRes(num-1) + getRes(num-2)
+}
+
+function runProgram(input) {
+    input = input.trim().split('\n')[0]
+    console.log(getRes(Number(input)))
+
+
+}
+
+if(process.env.USERNAME === "Himanshu"){
+    runProgram(`4`);
+}
+else{
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+    read += input;
+    });
+    process.stdin.on("end", function () {
+    read = read.replace(/\n$/,"")
+    runProgram(read);
+    });
+    process.on("SIGINT", function () {
+    read = read.replace(/\n$/,"")
+    runProgram(read);
+    process.exit(0);
+    });
+}
