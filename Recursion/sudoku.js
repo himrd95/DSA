7555ca9aedc126c846f45dcfe6ef83c95eca45f6
Himanshu <himrd95@gmail.com>
2021-09-06 15:26:16 +0530

    few changes

--- Recursion/sudoku.js
+++ Recursion/sudoku.js
@@ -1,10 +1,10 @@
 function ranges(val) {
 	const res = [];
-	if (val > 3) {
-		res.push(0, 3);
-	} else if (val > 6) {
+	if (val < 3) {
+		res.push(0, 2);
+	} else if (val < 6) {
 		res.push(3, 5);
-	} else {
+	} else if (val < 9) {
 		res.push(6, 8);
 	}
 	return res;
@@ -12,11 +12,12 @@ function ranges(val) {
 function canBePlaced(board, row, col, a) {
 	for (let i = 0; i < board.length; i++) {
 		if (board[i][col] == a) return false;
-		if (board[col][i] == a) return false;
+		if (board[row][i] == a) return false;
 	}
 	const row1 = ranges(row);
 	const col1 = ranges(row);
 
+	// console.log(row1, col1);
 	for (let i = row1[0]; i <= row1[1]; i++) {
 		for (let j = col1[0]; j <= col1[1]; j++) {
 			if (board[i][j] == a) {
@@ -32,20 +33,19 @@ function backTrack(board, row, col) {
 		col = 0;
 	}
 	if (row == 9) {
-		return;
+		return true;
 	}
+
 	if (board[row][col] != 0) {
 		return backTrack(board, row, col + 1);
 	}
-	for (let i = 1; i <= 9; i++) {
-		if (!canBePlaced(board, row, col, i)) {
-			continue;
-		}
-		board[row][col] = i;
-
-		backTrack(board, row, col + 1);
-		board[row][col] = 0;
+	for (let i = 1; i < 10; i++) {
+		if (!canBePlaced(board, row, col, i)) continue;
+		board[row][col] = `${i}`;
+		if (backTrack(board, row, col + 1)) return true;
+		board[row][col] = '0';
 	}
+	return false;
 }
 
 function runProgram(input) {
@@ -53,7 +53,7 @@ function runProgram(input) {
 
 	const board = [];
 	for (let i = 0; i < input.length; i++) {
-		board.push(input[i].trim().split(' ').map(Number));
+		board.push(input[i].trim().split(' '));
 	}
 	backTrack(board, 0, 0);
 	console.log(board);
