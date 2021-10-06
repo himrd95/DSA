e5fe90371a35c1362596831b2c77b57b8bde2189
Himanshu <himrd95@gmail.com>
2021-10-06 23:10:29 +0530

    new problem

---
+++ Revision/longestSubstringWithoutRepeatingChar.js
@@ -0,0 +1,31 @@
+var lengthOfLongestSubstring = function (s) {
+	s = s.split('');
+	let max = 0;
+	let i = 0,
+		j = 0;
+	const map = new Map();
+	// map.set(s[i], i);
+	while (j < s.length) {
+		// console.log(i, j, map.has(s[j]), map, s[j]);
+		if (map.has(s[j])) {
+			while (i <= j && s[i] !== s[j]) {
+				i++;
+			}
+			// map.delete(s[j]);
+			// i++;
+		}
+		map.set(s[j], j);
+		max = Math.max(max, j - i + 1);
+		j++;
+	}
+	return max;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
+
+/*
+i = 0 0 0 1 2
+j = 1 2 3
+max = 3
+
+*/
